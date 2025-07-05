670982f16808e5d85d66cb51c1205b899281e1e7
downYotube2548 <maneeohooho3790@gmail.com>
2025-07-05 14:31:11 +0700

    instancing api

--- src/main/java/io/downn_falls/libs/guiapi/GUIAPI.java
+++ src/main/java/io/downn_falls/libs/guiapi/GUIAPI.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GUIAPI extends JavaPlugin {
 
-    public static GUILibs register(Plugin plugin) {
+    public static GUILibs get(Plugin plugin) {
         return new GUILibs(plugin);
     }
 }
