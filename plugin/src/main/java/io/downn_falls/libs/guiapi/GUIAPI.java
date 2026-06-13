bff174ad5d7b5bcf46fab5a5bd52dde2691a94ad
downn_falls <maneeohooho3790@gmail.com>
2026-06-14 00:47:32 +0700

    fix: maven shade & lib management

--- plugin/src/main/java/io/downn_falls/libs/guiapi/GUIAPI.java
+++ plugin/src/main/java/io/downn_falls/libs/guiapi/GUIAPI.java
@@ -9,4 +9,16 @@ public final class GUIAPI extends JavaPlugin {
     public static GUILibs get(Plugin plugin) {
         return new GUILibs(plugin);
     }
+
+    private static GUILibs defaultLibs;
+
+    @Override
+    public void onEnable() {
+        defaultLibs = new GUILibs(this);
+        defaultLibs.register();
+    }
+
+    public static GUILibs getDefault() {
+        return defaultLibs;
+    }
 }
